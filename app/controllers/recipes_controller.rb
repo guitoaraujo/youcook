b3bb32bdef30f2ba0f0e127a02bfb124dbf79289
Igor Araujo <igor.araujo@xing.com>
2021-07-08 20:35:24 +0100

    adds most common tags feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,7 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipes::Search.new(recipes_params).call
     @filter = recipes_params[:filter]
+    @most_common_tags = count_tags
   end
 
   def show
@@ -14,5 +15,14 @@ class RecipesController < ApplicationController
 
   def recipes_params
     params.permit(:terms, :filter, :page)
+  end
+
+  def count_tags
+    tags = Recipe.all.map(&:ingredients).flatten
+    hash = Hash.new(0)
+    tags.each do |tag|
+      hash[tag] += 1
+    end
+    hash.sort_by { |_tag, n| n }.reverse.first(5)
   end
 end
